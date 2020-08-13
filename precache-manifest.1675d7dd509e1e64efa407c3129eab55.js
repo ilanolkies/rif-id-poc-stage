bfbf640a26e0f207747988be7dd83ee26d21f68c
Ilan Olkies <ilanolkies@outlook.com>
2020-08-13 17:17:02 -0300

    Updates

---
+++ precache-manifest.1675d7dd509e1e64efa407c3129eab55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b422779aeaaa9aadde96e4d81fe08ba",
+    "url": "/rif-id-poc-stage/index.html"
+  },
+  {
+    "revision": "3173cbb7ac19b8c6ac0a",
+    "url": "/rif-id-poc-stage/static/css/main.534ca111.chunk.css"
+  },
+  {
+    "revision": "c4372d7c2df3588b4565",
+    "url": "/rif-id-poc-stage/static/js/2.17c4b839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rif-id-poc-stage/static/js/2.17c4b839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3173cbb7ac19b8c6ac0a",
+    "url": "/rif-id-poc-stage/static/js/main.e9e7cd1b.chunk.js"
+  },
+  {
+    "revision": "19cc809460235af94b94",
+    "url": "/rif-id-poc-stage/static/js/runtime-main.b68f37ae.js"
+  },
+  {
+    "revision": "5e7b55e540d5791e3864378e19581d76",
+    "url": "/rif-id-poc-stage/static/media/logo.5e7b55e5.svg"
+  },
+  {
+    "revision": "0403ff1284005527203c8e427530ec8b",
+    "url": "/rif-id-poc-stage/static/media/power_by_iovlabs.0403ff12.svg"
+  }
+]);
